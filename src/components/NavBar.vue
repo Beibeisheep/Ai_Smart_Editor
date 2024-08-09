18c7151e9a0ffe3a7f7ff5b20588e46f41b49943
“zsh” <3563888138@qq.com>
2024-08-09 21:51:18 +0800

    实现登陆注册和退出登录

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,62 +1,56 @@
 <template>
-	<nav class="navbar navbar-expand-lg bg-light">
-		<div class="container-fluid">
-			<router-link class="navbar-brand" :to="{ name: 'LoginView' }">Gobang</router-link>
-			<!--使用a标签href跳转点完后会刷新，使用router-link就不会刷新-->
+	<n-layout>
+		<n-layout-header class="navbar">
+			<div class="container-fluid">
+				<router-link class="navbar-brand" :to="{ name: 'LoginView' }"> Editor </router-link>
 
-			<div class="collapse navbar-collapse" id="navbarText">
-				<ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-				<ul class="navbar-nav" v-if="$store.state.user.is_login">
-					<!--如果查询store里面的全局变量发现is_login是true-->
-					<li class="nav-item dropdown">
-						<a
-							class="nav-link dropdown-toggle"
-							href="#"
-							role="button"
-							data-bs-toggle="dropdown"
-							aria-expanded="false"
-						>
-							{{ $store.state.user.email }}
-						</a>
-						<ul class="dropdown-menu">
-							<router-link class="dropdown-item" :to="{ name: 'LoginView' }" @click="logout"
-								>退出登录</router-link
-							>
-						</ul>
-					</li>
-				</ul>
+				<div class="navbar-content">
+					<template v-if="$store.state.user.is_login">
+						<n-dropdown trigger="click">
+							<template #trigger>
+								<n-button text class="nav-link">
+									{{ $store.state.user.email }}
+								</n-button>
+							</template>
+							<n-dropdown-item @click="logout">退出登录</n-dropdown-item>
+						</n-dropdown>
+					</template>
 
-				<ul class="navbar-nav" v-else>
-					<!--如果发现没有登录-->
-					<li class="nav-item">
-						<router-link class="nav-link" :to="{ name: 'HomeView' }" role="button">
-							登录
+					<template v-else>
+						<router-link class="nav-link" :to="{ name: 'HomeView' }">
+							<n-button text>登录</n-button>
 						</router-link>
-					</li>
-					<li class="nav-item">
-						<router-link class="nav-link" :to="{ name: 'RegisterView' }" role="button">
-							注册
+						<router-link class="nav-link" :to="{ name: 'RegisterView' }">
+							<n-button text>注册</n-button>
 						</router-link>
-					</li>
-				</ul>
+					</template>
+				</div>
 			</div>
-		</div>
-	</nav>
+		</n-layout-header>
+	</n-layout>
 </template>
 
 <script>
-import { useRoute } from 'vue-router'
 import { computed } from 'vue'
 import { useStore } from 'vuex'
+import { useRoute } from 'vue-router'
+import { NLayout, NLayoutHeader, NButton, NDropdown } from 'naive-ui'
+import router from '../router/index'
 export default {
-	//导出模块
+	components: {
+		NLayout,
+		NLayoutHeader,
+		NButton,
+		NDropdown
+	},
 	setup() {
-		//setup是入口函数
-		const route = useRoute() //useRoute()必须在setup中使用，useRoute函数返回当前路由地址
-		let route_name = computed(() => route.name) //computed实时计算路由的名字
-		const store = useStore() //获取store,用来更新store里面属性的值
+		const route = useRoute()
+		const route_name = computed(() => route.name)
+		const store = useStore()
+
 		const logout = () => {
 			store.dispatch('logout')
+			router.push({ name: 'LoginView' })
 		}
 
 		return {
@@ -65,9 +59,7 @@ export default {
 		}
 	},
 	mounted() {
-		// 组件挂载后检查用户登录状态
 		if (!this.$store.state.user.is_login) {
-			// 如果未登录，则重定向到登录页面
 			this.$router.push({ name: 'LoginView' })
 		}
 	}
@@ -77,41 +69,44 @@ export default {
 <style scoped>
 .navbar {
 	box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.2);
+	background-color: #f8f9fa;
+	padding: 0 1rem;
+	position: fixed; /* Make the navbar fixed to the top */
+	top: 0; /* Align it to the top */
+	width: 100%; /* Full width */
+	height: 10%;
+	z-index: 1000; /* Ensure it stays on top of other content */
 }
 
 .container-fluid {
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
+	height: 100%;
 }
 
-.navbar-nav {
+.navbar-brand {
+	font-size: 1.8rem; /* Increase font size */
+	font-weight: bold;
+	color: #000;
+	text-decoration: none;
+	margin-top: 10px; /* Move down slightly */
+}
+
+.navbar-content {
 	display: flex;
 	align-items: center;
-	list-style: none; /* Remove list item markers */
-	padding-left: 0; /* Remove default padding */
-	margin-bottom: 0; /* Remove default margin */
-}
-
-.nav-item {
-	margin-left: 1rem;
 }
 
 .nav-link {
 	font-size: 1rem;
 	color: #000000ad;
+	margin-left: 1rem;
 	transition: color 0.3s ease;
 	text-decoration: none;
 }
 
 .nav-link:hover {
 	color: #13151a;
-}
-
-.navbar-brand {
-	font-size: 1.5rem;
-	font-weight: bold;
-	color: #000;
-	text-decoration: none; /* Remove underline from the brand link */
 }
 </style>
--- src/stores/user.js
+++ src/stores/user.js
@@ -1,63 +1,63 @@
-import $ from "jquery";
+import $ from 'jquery'
 export default {
-  state: {
-    email: "",
-    password: "",
-    is_login: false,
-  },
-  getters: {},
-  mutations: {
-    updateState(state, user) {
-      state.email = user.email;
-      state.password = user.password;
-      state.is_login = user.is_login;
-    },
-    // updateToken(state, token) {
-    //   state.token = token;
-    // },
-    logout(state) {
-      state.email = "";
-      state.password = "";
-      state.is_login = false;
-    },
-  },
-  actions: {
-    login(context, data) {
-      // 通过账号密码登录
-      $.ajax({
-        url: "http://10.6.3.167:8083/TextEditor/user/login",
-        type: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        data: JSON.stringify({
-          email: data.email,
-          password: data.password,
-        }),
-        success(resp) {
-          console.log("服务器响应:", resp); // 打印服务器响应
-          console.log("resp.message", resp.message);
-          if (resp.message == "登录成功") {
-            context.commit("updateState", {
-              email: resp.data.email,
-              password: resp.data.password,
-              is_login: true,
-            });
-            data.success(); // 调用传入的成功回调
-          } else {
-            console.log("登录失败信息:", resp);
-            data.error("用户名或密码错误");
-          }
-        },
-        error(resp) {
-          data.error(resp);
-        },
-      });
-    },
-    logout(context) {
-      // 退出登录
-      context.commit("logout");
-    },
-  },
-  modules: {},
-};
+	state: {
+		email: '',
+		password: '',
+		is_login: false
+	},
+	getters: {},
+	mutations: {
+		updateState(state, user) {
+			state.email = user.email
+			state.password = user.password
+			state.is_login = user.is_login
+		},
+		// updateToken(state, token) {
+		//   state.token = token;
+		// },
+		logout(state) {
+			state.email = ''
+			state.password = ''
+			state.is_login = false
+		}
+	},
+	actions: {
+		login(context, data) {
+			// 通过账号密码登录
+			$.ajax({
+				url: 'http://10.6.3.167:8083/TextEditor/user/login',
+				type: 'post',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				data: JSON.stringify({
+					email: data.email,
+					password: data.password
+				}),
+				success(resp) {
+					console.log('服务器响应:', resp) // 打印服务器响应
+					console.log('resp.message', resp.message)
+					if (resp.message == '执行成功') {
+						context.commit('updateState', {
+							email: resp.data.email,
+							password: resp.data.password,
+							is_login: true
+						})
+						data.success() // 调用传入的成功回调
+					} else {
+						console.log('登录失败信息:', resp)
+						data.error('用户名或密码错误')
+					}
+				},
+				error(resp) {
+					data.error(resp)
+				}
+			})
+		},
+		logout(context) {
+			// 退出登录
+			context.commit('logout')
+		}
+	},
+	modules: {}
+}
